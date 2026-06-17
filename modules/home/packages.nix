4f5e34b6ab5747f8b1e683bc0e419ea94a2b4263
Erased <kullolli.aaron@gmail.com>
2026-06-17 16:32:06 +0200

    update 1.0

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -3,13 +3,13 @@
   home.packages = with pkgs; [
     moonlight-qt
     rar
+    git
     pkgs.unstable.antigravity-cli
   ];
 
   programs = {
     fastfetch.enable = true;
     firefox.enable = true;
-    git.enable = true;
     mpv.enable = true;
     btop.enable = true;
     discord.enable = true;
